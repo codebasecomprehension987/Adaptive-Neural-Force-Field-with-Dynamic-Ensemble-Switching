de414900e0893972ce39c74b7ff0142166cbba55
codebasecomprehension987 <nonessentialtrying432@gmail.com>
2026-04-19 09:12:04 +0530

    feat: add core package init with forcefield and message passing exports

---
+++ src/core/init.py
@@ -0,0 +1,9 @@
+from .forcefield import AdaptiveNeuralForceField, AtomicSystem, ForceFieldOutput
+from .message_passing import MessagePassingLayer
+
+__all__ = [
+    "AdaptiveNeuralForceField",
+    "AtomicSystem",
+    "ForceFieldOutput",
+    "MessagePassingLayer",
+]
